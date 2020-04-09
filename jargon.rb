61b3920c4e51de9d74860c22721edefeab9d5096
clipperhouse <mwsherman@gmail.com>
2020-04-09 11:48:21 -0400

    Brew formula update for jargon version v0.9.15

--- jargon.rb
+++ jargon.rb
@@ -2,16 +2,16 @@
 class Jargon < Formula
   desc "Text pipelines for CLI and Go."
   homepage "https://github.com/clipperhouse/jargon"
-  version "0.9.14"
+  version "0.9.15"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/clipperhouse/jargon/releases/download/v0.9.14/jargon_0.9.14_Darwin_x86_64.tar.gz"
-    sha256 "6fd460e7756df70434b300ca944567df5f63dc73d0f81803dccc1d0b9c00cad5"
+    url "https://github.com/clipperhouse/jargon/releases/download/v0.9.15/jargon_0.9.15_Darwin_x86_64.tar.gz"
+    sha256 "148bcbd59fd1f679980b5c15cd7ae594df11af7113dc6355b9f285d4d1df24cf"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/clipperhouse/jargon/releases/download/v0.9.14/jargon_0.9.14_Linux_x86_64.tar.gz"
-      sha256 "72d05106d68ade3990a2ed80c7329e82a08eb653ac31b4132980b4c2d2511862"
+      url "https://github.com/clipperhouse/jargon/releases/download/v0.9.15/jargon_0.9.15_Linux_x86_64.tar.gz"
+      sha256 "78ccf1c387ea74dbda9b7c5c03097a7d9140375b0531ea7a1f2c2dc3105e81c6"
     end
   end
 
